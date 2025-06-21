6d2bfd0881ef136322a5fb9ed4f1e6fc66527590
kooljay11 <86942676+kooljay11@users.noreply.github.com>
2025-06-21 12:54:59 -0700

    Fixed relative times not being parsed to user local time

--- cogs/addsession.py
+++ cogs/addsession.py
@@ -27,7 +27,12 @@ class AddSession(commands.Cog):
                 user = await get_userinfo(user_id)
 
             try:
-                session_date_display = dateparser.parse(date)
+                user_timezone_for_parser = ''
+                if user["timezone"] > 0:
+                    user_timezone_for_parser += f'+'
+                user_timezone_for_parser += f'{int(user["timezone"]) * 100 + int(user["timezone"] % 1 * 60)}'
+
+                session_date_display = dateparser.parse(f'{date} {user_timezone_for_parser}')
                 session_date = await current_to_utc(session_date_display, user["timezone"])
             except:
                 await reply(self.client, interaction, f'Couldn\'t parse that date, sorry. Please check the external module documentation for further information: https://github.com/scrapinghub/dateparser')
